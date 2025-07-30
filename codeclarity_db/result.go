1a14adae3c6607b00d62dfcdd5086b1324247ea7
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-07-30 23:08:46 +0200

    Add CreatedOn field to Result struct with default timestamp

--- codeclarity_db/result.go
+++ codeclarity_db/result.go
@@ -1,6 +1,7 @@
 package codeclarity
 
 import (
+	"time"
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
@@ -11,4 +12,5 @@ type Result struct {
 	Result        interface{} `bun:"result"`
 	AnalysisId    uuid.UUID   `bun:"analysisId"`
 	Plugin        string      `bun:"plugin"`
+	CreatedOn     time.Time   `bun:"created_on,default:current_timestamp"`
 }
